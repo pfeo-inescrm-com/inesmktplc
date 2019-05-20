98e485fd8e6c0f2ae4f19363434cb33e5a9ef17b
pmfeo <pablo.feo.acosta@gmail.com>
2019-05-20 16:35:40 -0300

    Add social icons snippets template file
    -We can pick up the desired social network snippet from this template and add it to custom widget in footer area

--- themes/inesmktplc/searchform.php
+++ themes/inesmktplc/searchform.php
@@ -2,14 +2,9 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
     <div class="searc-wrap">
-        <input 
-            type="search"
-            id="<?php echo $unique_id; ?>"
-            class="search-field"
-            placeholder="<?php echo esc_attr_x( 'Search …', 'inesmktplc' ) ?>"
-            value="<?php echo get_search_query() ?>"
-            name="s"
-            title="<?php echo esc_attr_x( 'Search for:', 'inesmktplc' ) ?>" />
+        <input type="search" id="<?php echo $unique_id; ?>" class="search-field"
+            placeholder="<?php echo esc_attr_x( 'Search …', 'inesmktplc' ) ?>" value="<?php echo get_search_query() ?>"
+            name="s" title="<?php echo esc_attr_x( 'Search for:', 'inesmktplc' ) ?>" />
         <button type="submit" class="search-wrap__btn">
             <span class="lnr lnr-magnifier"></span>
         </button>
